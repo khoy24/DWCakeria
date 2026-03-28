5d0d47dab976f8dd05b027498c9463f4987d09cc
vanyllabel@gmail.com <>
2026-03-28 03:37:29 -0500

    Nav bar problems persist, scenes are updated

--- Assets/_Scripts/SceneLoader.cs
+++ Assets/_Scripts/SceneLoader.cs
@@ -5,6 +5,7 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        Debug.Log("Button clicked! Loading: " + sceneName);
         SceneManager.LoadScene(sceneName);
     }
 }
